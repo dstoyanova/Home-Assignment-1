2e354b2cdc11c9c25642cdbb2acb829030a218a6
Student22 <student22@mmadmins-Mac-mini-2.local>
2015-03-26 13:09:51 +0200

    Initial Commit

---
+++ Homework1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homework1
+//
+//  Created by Student22 on 3/26/15.
+//  Copyright (c) 2015 Student22. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Homework1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Homework1
+//
+//  Created by Student22 on 3/26/15.
+//  Copyright (c) 2015 Student22. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
